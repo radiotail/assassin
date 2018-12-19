a60078bd60597ed5ce481134b6b4c9f096214cd3
Unknown <radiotail86@gmail.com>
2018-12-19 15:17:33 +0800

    test

--- socks/socks5.go
+++ socks/socks5.go
@@ -1,10 +1,22 @@
 // RFC 1928 SOCKS Protocol Version 5
 package socks
-import "net"
+
+import (
+	"encoding/binary"
+	"log"
+	"net"
+	"strconv"
+)
 
 const ProtoVer = 5
 const NegotiationMethod = 0 // NO AUTHENTICATION REQUIRED
 const MaxBuffLen = 512
+
+const (
+	AType_IPV4 = 1
+	AType_DOMAIN = 3
+	AType_IPV6 = 4
+)
 
 //	recv:
 // +----+----------+----------+
@@ -40,24 +52,50 @@ func negotiation(client net.Conn) error {
 	return nil
 }
 
-+----+-----+-------+------+----------+----------+
-|VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
-+----+-----+-------+------+----------+----------+
-| 1  |  1  | X'00' |  1   | Variable |    2     |
-+----+-----+-------+------+----------+----------+
+// recv:
+// +----+-----+-------+------+----------+----------+
+// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+// +----+-----+-------+------+----------+----------+
+// | 1  |  1  | X'00' |  1   | Variable |    2     |
+// +----+-----+-------+------+----------+----------+
 
-+----+-----+-------+------+----------+----------+
-|VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
-+----+-----+-------+------+----------+----------+
-| 1  |  1  | X'00' |  1   | Variable |    2     |
-+----+-----+-------+------+----------+----------+
+// send:
+// +----+-----+-------+------+----------+----------+
+// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+// +----+-----+-------+------+----------+----------+
+// | 1  |  1  | X'00' |  1   | Variable |    2     |
+// +----+-----+-------+------+----------+----------+
 func request(client net.Conn) error  {
 	buffer := make([]byte, MaxBuffLen)
 
-	// read VER, NMETHODS
-	if _, err := client.Read(buffer[:2]); err != nil {
+	// read VER, REP, RSV, ATYP
+	if _, err := client.Read(buffer[:4]); err != nil {
 		return err
 	}
+
+	var host, port string
+	atyp := buffer[3]
+	switch atyp {
+	case AType_IPV4:
+		if _, err := client.Read(buffer[:net.IPv4len + 2]); err != nil {
+			return err
+		}
+
+		host = net.IP(buffer[:net.IPv4len]).String()
+		log.Printf("ipv4 host: %v, %v\n", host, buffer[:net.IPv4len])
+		port = strconv.Itoa(int(binary.LittleEndian.Uint16(buffer[net.IPv4len: net.IPv4len + 1])))
+	case AType_DOMAIN:
+	case AType_IPV6:
+		if _, err := client.Read(buffer[:net.IPv6len + 2]); err != nil {
+			return err
+		}
+
+		host = net.IP(buffer[:net.IPv6len]).String()
+		log.Printf("ipv6 host: %v, %v\n", host, buffer[:net.IPv6len])
+		port = strconv.Itoa(int(binary.LittleEndian.Uint16(buffer[net.IPv6len: net.IPv6len + 1])))
+	}
+
+	return nil
 }
 
 
